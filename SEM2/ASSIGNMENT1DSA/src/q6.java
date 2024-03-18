a7893edcd4fb60d01f6aa79ee1f44e5e0182adf0
SOYAM PARIDA <155126708+soyamparida@users.noreply.github.com>
2024-03-18 10:07:14 +0530

    Update q6.java

--- SEM2/ASSIGNMENT1DSA/src/q6.java
+++ SEM2/ASSIGNMENT1DSA/src/q6.java
@@ -1,9 +1,16 @@
-
+import java.util.Scanner;
 public class q6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+public static boolean isOdd(int n) {
+        return (n & 1) != 0;
+    }
 
-	}
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        System.out.print(n + " is odd: " + isOdd(n));
+    }
 }
