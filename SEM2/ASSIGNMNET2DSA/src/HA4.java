7cfcfadeae2e2b095037bdc1865e8738fba34f3b
SOYAM PARIDA <155126708+soyamparida@users.noreply.github.com>
2024-04-09 22:43:47 +0530

    Update HA4.java

--- SEM2/ASSIGNMNET2DSA/src/HA4.java
+++ SEM2/ASSIGNMNET2DSA/src/HA4.java
@@ -1,5 +1,4 @@
 
-	
 	class road { 
 		int meters ;
 		int centimeters ;
@@ -12,7 +11,6 @@
 		System.out.println("The distance in meters is " + meters);
 		System.out.println("The distance in centimeters is " + centimeters);
 	}
-	
 public int sum() {
 	return this.meters + this.centimeters ;
 }
