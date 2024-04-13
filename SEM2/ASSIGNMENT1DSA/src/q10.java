c5ed3f87023af704a38863f6b28d4d64c8c46960
SOYAM PARIDA <155126708+soyamparida@users.noreply.github.com>
2024-04-13 22:44:49 +0530

    Update q10.java

--- SEM2/ASSIGNMENT1DSA/src/q10.java
+++ SEM2/ASSIGNMENT1DSA/src/q10.java
@@ -2,37 +2,31 @@ import java.util.Scanner ;
 
 public class q10 {
 public static void main(String[] args) {
-Scanner  sc = new Scanner (System.in ) ; 
-System.out.println("EDnter the number os Elements of Array") ;
-int n = sc.nextInt(); 
-int array [] = new int [n] ; 
-System.out.println("Enter the elements of the array") ;
-for (int i = 0 ;  i <= array.length ; i++){
-	array[i] = sc.nextInt();
-}
-// seeing the occurcnce 
-int max = array[0] ;
-int min = array [0] ; 
-int maxcount =  0 ; 
-int mincount = 0 ; 
-int maxposition ; 
-int minposition ;
-for (int i = 0 ; i < n ; i ++) {
-	if(array[i]> max) {
-		max = array[i] ;
-		maxcount++ ; 
-		maxposition = i ; 
-	
-} ; 
-for (int i1 = 0 ; i1< n ; i1++) { 
-	if    (array[i1]< min) {
-		min = array[i1] ; 
-		mincount ++ ; 
-		minposition = i1 ; 
-	}
-}
-System.out.println("maximum element of the Array is" + max + "and " +"occutrens is"+ maxcount) ; 
-}
+  double sum = 0;
+        for (double[] row : m) {
+            sum += row[columnIndex];
+        }
 
-}
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int row = 3, col = 4;
+        double[][] arr = new double[row][col];
+
+        System.out.println("Enter a 3-by-4 matrix row by row: ");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                arr[i][j] = sc.nextDouble();
+            }
+        }
+
+        for (int i = 0; i < col; i++) {
+            System.out.println("Sum of the elements at column " + i + " is " + sumColumn(arr, i));
+        }
+
+        sc.close();
+    }
 }
